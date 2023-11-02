ed307ebe32907ad81125de036d172f5ac940a5e8
Yuliya Padzheva <yippee.smile@yandex.ru>
2023-11-02 12:12:44 +0300

    add scss and fix css

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-const popupOpenButton = document.querySelector('.nav__icon_burger');
+const popupOpenButton = document.querySelector('.icon_burger');
 const popupOpenButtonHeader = document.querySelector('.button_place_header');
-const popupCloseButton = document.querySelector('.popup__close_button');
+const popupCloseButton = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 
 
@@ -12,6 +12,6 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-popupOpenButton.addEventListener('click', openPopup);
 popupOpenButtonHeader.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
+popupOpenButton.addEventListener('click', openPopup);
